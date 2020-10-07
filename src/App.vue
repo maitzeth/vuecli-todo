cb68bbd6053dfe704e0184d29a6103aa10953b9a
maitzeth <andre801mz@gmail.com>
2020-10-07 09:43:45 -0300

    First commit :fire:

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,76 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <AddForm v-on:add-todo="handleAddTodo" />
+    <Todos :todos="todos" v-on:del-todo="handleDeleteTodo" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "./components/Layout/Header";
+import Todos from "./components/Todos";
+import AddForm from "./components/AddForm";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Header,
+    Todos,
+    AddForm,
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "Todo one",
+          completed: false,
+        },
+        {
+          id: 2,
+          title: "Todo two",
+          completed: false,
+        },
+        {
+          id: 3,
+          title: "Todo three",
+          completed: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    handleDeleteTodo(id) {
+      this.todos = this.todos.filter((item) => item.id !== id);
+    },
+    handleAddTodo(newTodo) {
+      this.todos = [...this.todos, newTodo];
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: sans-serif;
+}
+
+.btn {
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+
+.btn:hover {
+  background: #666;
 }
 </style>
---
+++ src/components/AddForm.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <form @submit.prevent="handleSubmit">
+      <input type="text" name="title" placeholder="Title..." v-model="title" />
+      <button type="submit" class="btn">Submit</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import { v4 as uuidv4 } from "uuid";
+
+export default {
+  name: "AddForm",
+  data() {
+    return {
+      title: "",
+    };
+  },
+  methods: {
+    handleSubmit() {
+      const newTodo = {
+        id: uuidv4(),
+        title: this.title,
+        completed: false,
+      };
+
+      this.$emit("add-todo", newTodo);
+
+      this.title = "";
+    },
+  },
+};
+</script>
+
+<style scoped>
+form {
+  display: flex;
+}
+
+input[type="text"] {
+  flex: 10;
+}
+
+input[type="submit"] {
+  flex: 2;
+}
+</style>
